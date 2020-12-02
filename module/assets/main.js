29d37734c2212f89cdd2855f39bf143bd9e85807
Martin Speer <martin.speer@accesso.com>
2020-12-02 11:00:36 -0800

    for sam

--- module/assets/main.js
+++ module/assets/main.js
@@ -1,7 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        "id": "sampleContextMenu",
-        "title": "Sample Context Menu",
-        "contexts": ["selection"]
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+        console.log("The color is green.");
     });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {pathContains: 'showare'},
+          })
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+      });
 });
