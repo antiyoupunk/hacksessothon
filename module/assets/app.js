a66310e926fa93970be7021c75f64f45e8eb5932
Martin Speer <martin.speer@accesso.com>
2020-12-02 15:45:37 -0800

    stuff

--- module/assets/app.js
+++ module/assets/app.js
@@ -1,32 +1,54 @@
 const con = new DBConnection;
+var appState = {currentName: "", currentStatus: "idle"};
 function getStatus(){
-    console.log(con.getStatus());
+    var status = con.getStatus();
+    if(status.user === ""){
+        chrome.browserAction.setIcon({path:"../icons/clear-128.png"});
+        appState.currentStatus = "idle";
+    }else{
+        if(status.user === appState.currentName){
+            chrome.browserAction.setIcon({path:"../icons/working-128.png"});
+            appState.currentStatus = "deploying";
+            checkPings();
+        }else{
+            chrome.browserAction.setIcon({path:"../icons/stop-128.png"});
+            appState.currentStatus = "blocked";
+        }
+    }
 }
+getStatus();
 window.setInterval(getStatus, 10000);
 function fetchName(){
     chrome.storage.sync.get({name: ''}, function(n) {
         updateName(n)
     });
 }
+function setDeploying(){
+    window.close();
+    if(appState.currentName === ""){
+        return;
+    }
+}
 function updateName(n){
     console.log(n)
-        if(n.name == '' || n.name == undefined || !n.name){
-            document.getElementById("username").innerHTML = '<a id="optionsLink">Set Name</a>';
-        }else{
-            console.log(n)
-            document.getElementById("username").innerHTML = '<a id="optionsLink">' + n.name + '</a>';
-        }
-        var optionsLink = document.getElementById('optionsLink');
-        optionsLink.addEventListener('click', function() {
-            chrome.tabs.create({ url: "../view/options.html" });
-        });
+    if(n.name == '' || n.name == undefined || !n.name){
+        appState.currentName = "";
+        document.getElementById("username").innerHTML = '<a id="optionsLink">Set Name</a>';
+    }else{
+        appState.currentName = n.name;
+        document.getElementById("username").innerHTML = '<a id="optionsLink">' + n.name + '</a>';
+    }
+    var optionsLink = document.getElementById('optionsLink');
+    optionsLink.addEventListener('click', function() {
+        chrome.tabs.create({ url: "../view/options.html" });
+    });
 }
 fetchName();
 document.addEventListener('DOMContentLoaded', function() {
     var deployButton = document.getElementById('setDeploying');
     
     deployButton.addEventListener('click', function() {
-        getStatus()
+        setDeploying()
     });
 
 });
