c150de6c148ca8f6e0c4b42ff01e15302166fd48
Martin Speer <martin.speer@accesso.com>
2020-12-02 11:18:46 -0800

    Update main.js

--- module/assets/main.js
+++ module/assets/main.js
@@ -1,6 +1,7 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
+        //intial check goes her
+        return;
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
