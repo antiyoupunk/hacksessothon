1206ec6e7cfb2ee45db40c6416ef8a8c57c55bdf
Sam Rudd <sam.rudd@accesso.com>
2020-12-02 12:23:58 -0800

    Update connect.js

--- module/assets/connect.js
+++ module/assets/connect.js
@@ -4,18 +4,19 @@ var DBConnection = function() {};
 
 DBConnection.prototype.getStatus = function() {
 	let request = new XMLHttpRequest();
-	request.open("GET", url);
+	var returnObj = {};
+	request.open("GET", url, false);
 	request.send();
-	request.onload = () => {
-		if (request.status === 200) {
-			statusObj = JSON.parse(request.response);
-			returnObj = {
-				user: statusObj['user'],
-				status: statusObj['status']
-			};
-			return returnObj;
-		} else {
-			console.log("Error: " + request.status + " " + request.statusText);
-		}
-	};
+	
+	if (request.readyState === 4) {
+		statusObj = JSON.parse(request.response);
+		returnObj = {
+			user: statusObj['user'],
+			status: statusObj['status']
+		};
+	} else {
+		console.log("Error: " + request.status + " " + request.statusText);
+	}
+	
+	return returnObj;
 };
